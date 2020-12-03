556750787258e1082b7f8fa11bf92129ef43f2d2
Nicusor Nedelcu <nnedelcu@beamng.com>
2020-12-03 11:17:42 +0200

    wip

--- src/engine/tilemap_layer_tiles.h
+++ src/engine/tilemap_layer_tiles.h
@@ -7,6 +7,7 @@ namespace engine
 struct TilemapLayerTiles : Unit
 {
 	struct TilemapLayer* tilemapLayer = nullptr;
+	Vec2 imageScale = { 1, 1 };
 
 	virtual void update(struct Game* game) override;
 	virtual void render(struct Graphics* gfx) override;
