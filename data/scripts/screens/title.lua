ef2c4cdc0d58c4c89c96e97764bd2127f2f1a792
Nicusor Nedelcu <nnedelcu@beamng.com>
2021-02-12 03:51:42 +0200

    wip

--- data/scripts/screens/title.lua
+++ data/scripts/screens/title.lua
@@ -3,12 +3,13 @@ local C = {}
 local zoom = 1
 
 local center = Vec2(120, 150)
+local pan = Vec2(0, 0)
 
 local function project(x, y, z, center)
   local screen = Vec2()
   if z > 0 then
-      screen.x = x / z * zoom + center.x
-      screen.y = y / z * zoom + center.y
+      screen.x = (x + pan.x) / z * zoom + center.x
+      screen.y = (y + pan.y) / z * zoom + center.y
   end
   return screen
 end
@@ -22,18 +23,20 @@ function C:onUpdate()
   --game:changeMainScript("scripts/ingame_screen")
 end
 local offs = 0.1
+local loffs = 0
 local step = 80
 local y = 20
 function C:onRender()
   self.palette:setColor(253, Color(self.c, self.c, self.c, self.c))
   self.c = self.c + game.deltaTime/3
+  pan.x = pan.x + math.sin(self.c)/25
   --gfx:drawPalettedSprite(self.titleSpr, Rect(gfx.videoWidth / 2 - 150/2, 0, 150, 150), 0, 0, self.palette)
   --gfx:drawPalettedSpriteCustomQuad(self.titleSpr, Vec2(10, 10),Vec2(100, 30),Vec2(100+self.c*10, 130+self.c*10),Vec2(10,130), 0, 0, self.palette)
-  for i = 0, 10 do
-    local p0 = {x = 70, y = -500, z = 2}
-    local p1 = {x = 70, y = -500, z = 1}
-    local p2 = {x = 70, y = -500 + 80, z = 1}
-    local p3 = {x = 70, y = -500 + 80, z = 2}
+  for i = 0, 20 do
+    local p0 = {x = 70, y = -1500, z = 2}
+    local p1 = {x = 70, y = -1500, z = 1}
+    local p2 = {x = 70, y = -1500 + 80, z = 1}
+    local p3 = {x = 70, y = -1500 + 80, z = 2}
     p0.y = p0.y + offs + step * i
     p1.y = p1.y + offs + step * i
     p2.y = p2.y + offs + step * i
@@ -43,15 +46,14 @@ function C:onRender()
     local pr1 = project(p1.x, p1.y, p1.z, center)
     local pr2 = project(p2.x, p2.y, p2.z, center)
     local pr3 = project(p3.x, p3.y, p3.z, center)
-
     gfx:drawSpriteCustomQuad(self.rocksSpr, pr0, pr1, pr2, pr3, 0, 0)
   end
 
-  for i = 0, 10 do
-    local p0 = {x = -70, y = -500, z = 2}
-    local p1 = {x = -70, y = -500, z = 1}
-    local p2 = {x = -70, y = -500 + 80, z = 1}
-    local p3 = {x = -70, y = -500 + 80, z = 2}
+  for i = 0, 20 do
+    local p0 = {x = -70, y = -1500, z = 2}
+    local p1 = {x = -70, y = -1500, z = 1}
+    local p2 = {x = -70, y = -1500 + 80, z = 1}
+    local p3 = {x = -70, y = -1500 + 80, z = 2}
     p0.y = p0.y + offs + step * i
     p1.y = p1.y + offs + step * i
     p2.y = p2.y + offs + step * i
@@ -62,13 +64,53 @@ function C:onRender()
     local pr2 = project(p2.x, p2.y, p2.z, center)
     local pr3 = project(p3.x, p3.y, p3.z, center)
     gfx:drawSpriteCustomQuad(self.rocksSpr, pr0, pr1, pr2, pr3, 0, 0)
-
     local pf0 = Vec2(pr0.x, pr0.y)
     local pf1 = Vec2(pr0.x + 70, pr0.y)
     local pf2 = Vec2(pr3.x + 70, pr3.y)
     local pf3 = Vec2(pr3.x, pr3.y)
-
     gfx:drawSpriteCustomQuad(self.lavaSpr, pf0, pf1, pf2, pf3, 0, 0)
+  end
+
+
+  for i = 0, 20 do
+    local p0 = {x = 70, y = -1500, z = 2}
+    local p1 = {x = 70, y = -1500, z = 1}
+    local p2 = {x = 70, y = -1500 + 80, z = 1}
+    local p3 = {x = 70, y = -1500 + 80, z = 2}
+    p0.y = p0.y + offs + step * i
+    p1.y = p1.y + offs + step * i
+    p2.y = p2.y + offs + step * i
+    p3.y = p3.y + offs + step * i
+
+    local pr0 = project(p0.x, p0.y, p0.z, center)
+    local pr1 = project(p1.x, p1.y, p1.z, center)
+    local pr2 = project(p2.x, p2.y, p2.z, center)
+    local pr3 = project(p3.x, p3.y, p3.z, center)
+    local pf0 = Vec2(pr1.x-10, pr1.y)
+    local pf1 = Vec2(pr1.x + 70, pr1.y)
+    local pf2 = Vec2(pr2.x + 70, pr2.y)
+    local pf3 = Vec2(pr2.x -10, pr2.y)
+    gfx:drawSpriteCustomQuad(self.iceSpr, pf0, pf1, pf2, pf3, 0, 0)
+  end
+
+  for i = 0, 20 do
+    local p0 = {x = -70, y = -1500, z = 2}
+    local p1 = {x = -70, y = -1500, z = 1}
+    local p2 = {x = -70, y = -1500 + 80, z = 1}
+    local p3 = {x = -70, y = -1500 + 80, z = 2}
+    p0.y = p0.y + offs + step * i
+    p1.y = p1.y + offs + step * i
+    p2.y = p2.y + offs + step * i
+    p3.y = p3.y + offs + step * i
+    local pr0 = project(p0.x, p0.y, p0.z, center)
+    local pr1 = project(p1.x, p1.y, p1.z, center)
+    local pr2 = project(p2.x, p2.y, p2.z, center)
+    local pr3 = project(p3.x, p3.y, p3.z, center)
+    pf0 = Vec2(pr1.x+10, pr1.y)
+    pf1 = Vec2(pr1.x - 70, pr1.y)
+    pf2 = Vec2(pr1.x - 70, pr2.y)
+    pf3 = Vec2(pr2.x+10, pr2.y)
+    gfx:drawSpriteCustomQuad(self.iceSpr, pf0, pf1, pf2, pf3, 0, 0)
   end
 
   offs = offs + game.deltaTime * 55
@@ -79,6 +121,7 @@ function C:onScreenEnter()
   self.titleSpr = game:loadSprite("sprites/title")
   self.rocksSpr = game:loadSprite("sprites/rocks")
   self.lavaSpr = game:loadSprite("sprites/lava")
+  self.iceSpr = game:loadSprite("sprites/ice")
   self.palette = gfx:createUserPalette()
   self.palette:copyFromSprite(self.titleSpr)
 end
