7305759d4139200b1a2a6011daea9ff193c369df
nekitu <webnktu@gmail.com>
2022-06-15 17:52:32 +0300

    wip

---
+++ libsold/sdl2/include/SDL_revision.h
@@ -0,0 +1,2 @@
+#define SDL_REVISION "http://github.com/nekitu/shmup.git@4267a9efc313fe524d551cf7625083fcf7960acc"
+#define SDL_REVISION_NUMBER 0
---
+++ libsold/sdl_mixer/build/.libs/music_cmd.d
@@ -0,0 +1,5 @@
+build/music_cmd.lo: music_cmd.c /usr/include/SDL2/SDL_config.h \
+ music_cmd.h music.h SDL_mixer.h /usr/include/SDL2/SDL_stdinc.h \
+ /usr/include/SDL2/SDL_rwops.h /usr/include/SDL2/SDL_audio.h \
+ /usr/include/SDL2/SDL_endian.h /usr/include/SDL2/SDL_version.h \
+ /usr/include/SDL2/begin_code.h /usr/include/SDL2/close_code.h
