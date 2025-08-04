e456b69bc4e0baca2457a16e8f269706835c99e0
Nicusor Nedelcu <nnedelcu@beamng.com>
2025-08-04 11:51:44 +0300

    wip

--- generate.cmd
+++ generate.cmd
@@ -1,4 +1,4 @@
 mkdir build
 cd ./build
-cmake -G "Visual Studio 17 2022" -DOpenGL_GL_PREFERENCE=GLVND ../
+cmake -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -G "Visual Studio 17 2022" -DOpenGL_GL_PREFERENCE=GLVND ../
 cd ..
